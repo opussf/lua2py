9fa12088e09025d64b242224044cb01837652544
opussf <github@zz9-za.com>
2026-02-08 14:15:56 -0800

    Tests and it works, sort of

--- src/lua2py/dump/dumps.py
+++ src/lua2py/dump/dumps.py
@@ -1,30 +1,39 @@
 from typing import Any
 
-def dumps(obj_in: Any) -> str:
-    if isinstance(obj_in, list):
-        return __dumps_list(obj_in)
-    if isinstance(obj_in, dict):
-        return __dumps_dict(obj_in)
+class dumps:
+    def __new__(cls, obj_in:Any) -> str:
+        """__init__ returns an object, __new__ can return a value"""
+        instance = super().__new__(cls)
+        instance.list_out = []
 
-def __dumps_any(obj_in: Any) -> str:
-    if isinstance(obj_in, bool):
-        print(f"{obj_in} is a bool.")
-    elif isinstance(obj_in, int):
-        print(f"{obj_in} is an int.")
-    elif isinstance(obj_in, str):
-        print(f"{obj_in} is a str.")
-    elif isinstance(obj_in, list):
-        print(f"{obj_in} is a list.")
-    elif isinstance(obj_in, dict):
-        print(f"{obj_in} is a dict.")
-    elif obj_in is None:
-        print(f"{obj_in} is a None")
+        instance.__parse_obj(obj_in)
+        print(instance.list_out)
 
-def __dumps_list(list_in: list) -> str:
-    print("I am a list!")
-    for i in list_in:
-        print(i, type(i))
-        __dumps_any(i)
+        return "".join(instance.list_out)
 
-def __dumps_dict(dict_in: dict) -> str:
-    print("I am a dict")
+    def __parse_obj(instance, obj_in:Any) -> None:
+        if isinstance(obj_in, bool):
+            instance.list_out.append(obj_in and "true" or "false")
+        elif isinstance(obj_in, int):
+            instance.list_out.append(str(obj_in))
+        elif isinstance(obj_in, str):
+            instance.list_out.append(f'"{obj_in}"')
+        elif isinstance(obj_in, list):
+            instance.list_out.append("{")
+            instance.__dumps_list(obj_in)
+        elif isinstance(obj_in, dict):
+            instance.list_out.append("{")
+            instance.__dumps_dict(obj_in)
+        elif obj_in is None:
+            instance.list_out.append("nil")
+
+    def __dumps_list(instance, list_in: list) -> None:
+        print(f"{list_in} is a list!")
+        for item in list_in:
+            instance.__parse_obj(item)
+            instance.list_out.append(",")
+
+    def __dumps_dict(instance, dict_in: dict) -> None:
+        print(f"{dict_in} is a dict.")
+        for key, value in dict_in.items():
+            print(f"[{key}] = {value}")
--- tests/test_py2lua.py
+++ tests/test_py2lua.py
@@ -28,12 +28,33 @@ class Test_py2lua:
 
 	def test_dumps_dict_to_lua_multiple(self):
 		data = lua2py.dumps({"k1": "v1", "k2": "v2"})
-		assert 5 == 5   # how to test this?
+		assert data == "Write me"
 
-	def test_dump_dict_to_lua_mixed(self):
+	def test_dumps_dict_to_lua_mixed(self):
 		data = lua2py.dumps({"1": 1, "2": True, "3": False, "4": "4", "k1": "v1", "k2": 5})
-		assert 5 == 5
+		assert data == "Write me"
 
-	def test_dump_dict_with_embedded_dict(self):
+	def test_dumps_dict_with_embedded_dict(self):
 		data = lua2py.dumps({"5": {"10": 42}})
 		assert data == "{[\"5\"] = {[\"10\"] = 42}}"
+
+	###################  Primatives
+	def test_dumps_bool_true(self):
+		data = lua2py.dumps(True)
+		assert data == "true"
+
+	def test_dumps_bool_false(self):
+		data = lua2py.dumps(False)
+		assert data == "false"
+
+	def test_dumps_int_only(self):
+		data = lua2py.dumps(42)
+		assert data == "42"
+
+	def test_dumps_float_only(self):
+		data = lua2py.dumps(3.1415927)
+		assert data == 3.1415927
+
+	def test_dumps_str_only(self):
+		data = lua2py.dumps("lua rules")
+		assert data == '"lua rules"'
