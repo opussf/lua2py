4d0a99e855d772e046d833d5aff91818d913ba11
opussf <github@zz9-za.com>
2026-02-10 14:16:40 -0800

    Dump test, and works?

--- src/lua2py/dump/dump.py
+++ src/lua2py/dump/dump.py
@@ -1,5 +1,7 @@
 from typing import Any, IO
+import lua2py
 
 
 def dump(obj_in: Any, file_in: IO[str]) -> None:
-    pass
+    with open(file_in, "w", encoding="utf-8") as fh:
+        fh.write(lua2py.dumps(obj_in))
